7803f9089f71034e8ddd737b68aa8a56303e2095
won9-dev <wonho.jung92@gmail.com>
2021-07-16 22:34:08 -0400

    summary, handleclickdisplay

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { markdown } from 'markdown';
 function Post({ data, title, body, author, publishData }) {
-  console.log(markdown.toHTML(body));
+  //Change state of display body content
+  const [block, setBlock] = useState('none');
+
+  //get summary
+  const summary = body.split('##');
+  console.log(summary[0]);
+
+  //After Click, display body on the screen.
+  const handleClickDisplay = () => {
+    if (block === 'none') {
+      setBlock('block');
+    } else {
+      setBlock('none');
+    }
+  };
   return (
     <div>
-      <h1>Title:{title}</h1>
-      <h1>Author:{author}</h1>
-      <h1>Date:{publishData}</h1>
+      <h1 onClick={handleClickDisplay}>Title:{title}</h1>
+      <h3>Author:{author}</h3>
+      <p>Date:{publishData}</p>
+      <h4> Summary: {summary && summary[0].substr(1)}</h4>
       <div
+        style={{ display: block }}
         dangerouslySetInnerHTML={{
           __html: markdown.toHTML(body),
         }}
